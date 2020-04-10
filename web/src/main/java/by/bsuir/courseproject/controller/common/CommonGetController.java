3b4044d8ceb6683898d192a9b087b96d2ec51a67
Artsiom Sadouski <artsiom_sadouski@epam.com>
2020-04-10 14:52:12 +0300

    Fixes

--- web/src/main/java/by/bsuir/courseproject/controller/common/CommonGetController.java
+++ web/src/main/java/by/bsuir/courseproject/controller/common/CommonGetController.java
@@ -10,6 +10,7 @@ import by.bsuir.courseproject.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ import java.util.Optional;
 
 
 @RestController
+@Component
 public class CommonGetController {
 
     private StudentService studentService;
