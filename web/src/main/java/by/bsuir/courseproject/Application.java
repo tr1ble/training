499b457438b4fe57f083ddc4c5db1ed76de6d0d2
Artsiom Sadouski <artsiom_sadouski@epam.com>
2020-04-10 13:18:29 +0300

    Database config is changed

--- web/src/main/java/by/bsuir/courseproject/Application.java
+++ web/src/main/java/by/bsuir/courseproject/Application.java
@@ -18,7 +18,7 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/*").allowedOrigins("http://https://training-online.herokuapp.com/*");
             }
         };
     }
