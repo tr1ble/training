b692d17a7f8469e5c1b2397393dfb1ce77d157f0
Artsiom Sadouski <artsiom_sadouski@epam.com>
2020-04-22 01:06:13 +0300

    Registration of user is fixed

--- repository-api/src/main/java/by/bsuir/courseproject/repository/CourseRepository.java
+++ repository-api/src/main/java/by/bsuir/courseproject/repository/CourseRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends CrudRepository<Course, Integer> {
+    Optional<Course> findById(int id);
     List<Course> findAll();
     List<Course> findByTrainer(Trainer trainer);
 }
---
+++ web/src/main/java/by/bsuir/courseproject/controller/common/CommonAddController.java
@@ -0,0 +1,42 @@
+package by.bsuir.courseproject.controller.common;
+
+import by.bsuir.courseproject.entites.Role;
+import by.bsuir.courseproject.entites.User;
+import by.bsuir.courseproject.service.completedtask.CompletedTaskService;
+import by.bsuir.courseproject.service.course.CourseService;
+import by.bsuir.courseproject.service.student.StudentService;
+import by.bsuir.courseproject.service.task.TaskService;
+import by.bsuir.courseproject.service.trainer.TrainerService;
+import by.bsuir.courseproject.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class CommonAddController {
+
+    private UserService userService;
+    @Autowired
+    public CommonAddController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = {"/register"}, method = RequestMethod.POST)
+    public ResponseEntity register(@RequestBody(required = false) User user) {
+        Optional<User> userOptional = userService.getUserByLogin(user.getLogin());
+        if(!userOptional.isPresent()) {
+            user.setRole(Role.valueOf("ROLE_STUDENT"));
+            userService.add(user);
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("This login is already taken", HttpStatus.OK);
+        }
+
+    }
+}
