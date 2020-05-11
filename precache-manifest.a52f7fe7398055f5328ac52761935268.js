a7451751a51d7f7c2497e41943dfabaaffa53b09
Artsiom Sadouski <artsiom_sadouski@epam.com>
2020-05-11 12:26:42 +0300

    Updates

---
+++ precache-manifest.a52f7fe7398055f5328ac52761935268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccc22c63ca38f7d44cf17c77bec5624",
+    "url": "/training/index.html"
+  },
+  {
+    "revision": "b29512118547516dfc94",
+    "url": "/training/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "9b051b3df5fceb53760d",
+    "url": "/training/static/css/main.ecf3ad5e.chunk.css"
+  },
+  {
+    "revision": "b29512118547516dfc94",
+    "url": "/training/static/js/2.69f8d8e3.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/training/static/js/2.69f8d8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b051b3df5fceb53760d",
+    "url": "/training/static/js/main.1226b1b5.chunk.js"
+  },
+  {
+    "revision": "e1dc063f4ec2fa529011",
+    "url": "/training/static/js/runtime-main.bacf04e7.js"
+  }
+]);
